bfc810b0543548464631ac552539a739c0f8f681
DucHoang <ndhdldt1@gmail.com>
2019-08-29 14:54:34 +0700

    Complete models: Order and State

--- models/Order.js
+++ models/Order.js
@@ -2,6 +2,20 @@ var mongoose        = require('mongoose'),
     uniqueValidator = require('mongoose-unique-validator');
 
 var OrderSchema = new mongoose.Schema({
+    id_customer: { type: String },
+    products   : [{
+        category: { type: String },
+        suite   : { type: String },
+        embryos : { type: String },
+        size    : { type: String },
+        state   : { type: Number, min: 0, max: 1, default: 0 },
+        type    : { type: String, enum: ['new', 'old'], default: 'new'},
+        label   : { type: String }
+    }],
+    credit     : {},
+    note       : { type: String },
+    state      : { type: String }  
+
 }, {timestamps: true});
 
 OrderSchema.plugin(uniqueValidator, 'is already exist.');
--- models/State.js
+++ models/State.js
@@ -2,6 +2,11 @@ var mongoose        = require('mongoose'),
     uniqueValidator = require('mongoose-unique-validator');
 
 var StateSchema = new mongoose.Schema({
+    id         : { type: String, upperCase: true, trim: true, required: true, unique: true },
+    name       : { type: String },
+    description: { type: String },
+    next       : { type: String }
+
 }, {timestamps: true});
 
 StateSchema.plugin(uniqueValidator, 'is already exist.');
